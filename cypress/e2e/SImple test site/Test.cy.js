85b917f98dd5164ca0955d6352dbfe5b0f7503a8
marcingruszczyk <marcin.gruszczyk95@gmail.com>
2023-04-12 19:02:48 +0200

    add hovers and dropdownlist testes

--- cypress/e2e/SImple test site/Test.cy.js
+++ cypress/e2e/SImple test site/Test.cy.js
@@ -1,6 +1,8 @@
 import InputPage from "../../page-objects/inputPage";
 import HomePage from "../../page-objects/homePage";
 import CheckboxPage from "../../page-objects/checkboxPage";
+import HoversPage from "../../page-objects/hoversPage";
+import DropdownListPage from "../../page-objects/dropdownListPage";
 
 describe("Test of simple website", () => {
   beforeEach(() => {
@@ -23,5 +25,19 @@ describe("Test of simple website", () => {
     const checkbox = new CheckboxPage();
     checkbox.checkFirstCheckbox();
     checkbox.uncheckLastCheckbox();
+  });
+  it("test dropdown list", function () {
+    const homePage = new HomePage();
+    homePage.clickDropdownListTab();
+
+    const dropdownPage = new DropdownListPage();
+    dropdownPage.chooseFirstOptionFromDropdownList();
+  });
+  it("test hover", function () {
+    const homePage = new HomePage();
+    homePage.clickHoversTab();
+
+    const hoversPage = new HoversPage();
+    hoversPage.hoverOverElement();
   });
 });
